18f044ae2c00927f8784f0d87d89228e66936a05
mrwhoareyou <2359146115@qq.com>
2018-01-23 22:19:07 +0800

    23 Merge k Sorted Lists

---
+++ Algorithms/23 Merge k Sorted Lists/index.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+// 最简单的方法，每次取最小的元素，依次构建新的链表，可能会超时。时间复杂度O(mn)
+var mergeKLists = function(lists) {
+	var i, min, minIndex,
+		head = p = null,
+		len = lists.length;
+	
+	while(true) {
+		minIndex = -1;
+		min = Infinity;
+
+		for(i = 0;i < len;i++) {
+			if(lists[i] != null) {
+				if(lists[i].val < min) {
+					min = lists[i].val;
+					minIndex = i;
+				}
+			}
+		}
+
+		if(min < Infinity) {
+			if(head == null) {
+				head = p = new ListNode(min);
+			}
+			else {
+				p.next = new ListNode(min);
+				p = p.next;
+			}
+
+			lists[minIndex] = lists[minIndex].next;
+		}
+		else {
+			return head;
+		}
+	}
+};
