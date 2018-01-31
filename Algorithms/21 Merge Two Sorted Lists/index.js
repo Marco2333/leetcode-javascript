2aff31f3a655223f8651125f799c3af5384a2c6b
mrwhoareyou <2359146115@qq.com>
2018-01-31 16:13:20 +0800

    Merge k Sorted Lists

--- Algorithms/21 Merge Two Sorted Lists/index.js
+++ Algorithms/21 Merge Two Sorted Lists/index.js
@@ -65,3 +65,39 @@ var mergeTwoLists = function(l1, l2) {
 
 	return head;
 };
+
+
+var mergeTwoLists = function(l1, l2) {
+	var val, head = p = null;
+
+	while(l1 != null || l2 != null) {
+		if(l1 != null && l2 != null) {
+			if(l1.val <= l2.val) {
+				val = l1.val;
+				l1 = l1.next;
+			}
+			else {
+				val = l2.val;
+				l2 = l2.next;
+			}
+		}
+		else if(l1 != null) {
+			val = l1.val;
+			l1 = l1.next;
+		}
+		else {
+			val = l2.val;
+			l2 = l2.next;
+		}
+
+		if(head == null) {
+			head = p = new ListNode(val);
+		}
+		else {
+			p.next = new ListNode(val);
+			p = p.next;
+		}
+	}
+
+	return head;
+};
--- Algorithms/23 Merge k Sorted Lists/index.js
+++ Algorithms/23 Merge k Sorted Lists/index.js
@@ -9,7 +9,7 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
-// 最简单的方法，每次取最小的元素，依次构建新的链表，可能会超时。时间复杂度O(mn)
+// 最简单的方法，每次取最小的元素，依次构建新的链表，可能会超时。时间复杂度O(nk^2)
 var mergeKLists = function(lists) {
 	var i, min, minIndex,
 		head = p = null,
@@ -48,4 +48,63 @@ var mergeKLists = function(lists) {
 			return head;
 		}
 	}
+};
+
+
+var mergeTwoLists = function(l1, l2) {
+	var val, head = p = null;
+
+	while(l1 != null || l2 != null) {
+		if(l1 != null && l2 != null) {
+			if(l1.val <= l2.val) {
+				val = l1.val;
+				l1 = l1.next;
+			}
+			else {
+				val = l2.val;
+				l2 = l2.next;
+			}
+		}
+		else if(l1 != null) {
+			val = l1.val;
+			l1 = l1.next;
+		}
+		else {
+			val = l2.val;
+			l2 = l2.next;
+		}
+
+		if(head == null) {
+			head = p = new ListNode(val);
+		}
+		else {
+			p.next = new ListNode(val);
+			p = p.next;
+		}
+	}
+
+	return head;
+};
+
+// 归并法，时间复杂度O(nklogk)，n为链表的平均长度
+var mergeKLists = function(lists) {
+	var i, len,
+		resArr = [];
+
+	if(lists == null || lists.length === 0) {
+		return null;
+	}
+
+	while(lists.length !== 1) {
+		len = lists.length;
+
+		for(i = 0;i < len;i += 2) {
+			resArr.push(mergeTwoLists(lists[i], lists[i + 1]));
+		}
+
+		lists = resArr;
+		resArr = [];
+	}
+
+	return lists[0];
 };
