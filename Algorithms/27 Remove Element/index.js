2beee638abc3cd25c02d10fda276b2b7fac5748e
mrwhoareyou <2359146115@qq.com>
2018-02-13 11:27:24 +0800

    27 Remove Element

---
+++ Algorithms/27 Remove Element/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+	var i;
+	
+	for(i = 0;i < nums.length;i++) {
+		if(nums[i] === val) {
+			nums.splice(i, 1);
+			i--
+		}
+	}
+
+	return nums.length;
+};
